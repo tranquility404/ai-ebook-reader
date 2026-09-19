eff861086140f0baf403264a72fdb96d8b634884
Aman Verma <deepak.2004.verma@gmail.com>
2026-09-20 01:46:34 +0530

    not maintaining any more

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from "vite-tsconfig-paths"
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), 
-    tsconfigPaths(),
-    tailwindcss()
-  ],
-  server: {
-    port: 3000,
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      tailwindcss()
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.BACKEND_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+});
